b114e37d34ba888524ad24d8e473cba0a93357e3
Richard Neves <ricknevesbc@icloud.com>
2024-07-11 11:28:11 -0300

    feat: Add create a new trip

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,24 @@
+import cors from '@fastify/cors'
 import fastify from 'fastify'
+import {
+  serializerCompiler,
+  validatorCompiler,
+} from 'fastify-type-provider-zod'
+import { env } from './env'
+import { confirmTrip } from './routes/confirm-trip'
+import { createTrip } from './routes/create-trip'
 
 const app = fastify()
 
-app.listen({ port: 3333 }, () => {
-  console.log('Server running')
+app.register(cors, {
+  origin: '*',
+})
+
+app.setValidatorCompiler(validatorCompiler)
+app.setSerializerCompiler(serializerCompiler)
+
+app.register(createTrip)
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log('Server running!')
 })
