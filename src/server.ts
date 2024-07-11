a1ca2712413bc90394e39a0897037ab69ddf01f1
Richard Neves <ricknevesbc@icloud.com>
2024-07-11 11:43:39 -0300

    feat: Add confirm trip and participants

--- src/server.ts
+++ src/server.ts
@@ -5,6 +5,7 @@ import {
   validatorCompiler,
 } from 'fastify-type-provider-zod'
 import { env } from './env'
+import { confirmParticipant } from './routes/confirm-participant'
 import { confirmTrip } from './routes/confirm-trip'
 import { createTrip } from './routes/create-trip'
 
@@ -18,6 +19,8 @@ app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
 
 app.register(createTrip)
+app.register(confirmTrip)
+app.register(confirmParticipant)
 
 app.listen({ port: env.PORT }).then(() => {
   console.log('Server running!')
